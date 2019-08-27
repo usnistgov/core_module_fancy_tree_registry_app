7daa9c05f43f9d71635930bd13df2d1fbb66474e
Augustin CHINI <augustin.chini@nist.gov>
2019-08-27 10:07:29 -0400

    chore: upgrade 1.5.0
    Change-Id: I9f8878c31883b2de80524f192d56e27d7653cb9c

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_module_fancy_tree_registry_app",
-    version="1.4.0",
+    version="1.5.0",
     description="Fancy Tree module for the parser core project",
     long_description=long_desc,
     author="NIST IT Lab",
