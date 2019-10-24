8f8e4525975c389a0844c01289a7f948eb95fcac
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-10-24 14:50:31 -0400

    fix: revert django upgrade
    Change-Id: I25a832727d33ec7448e52d0b8ecd9b7f5d62f977

--- core_module_fancy_tree_registry_app/urls.py
+++ core_module_fancy_tree_registry_app/urls.py
@@ -1,12 +1,11 @@
 """ Url router for the fancy tree module
 """
-
-from django.urls import re_path
+from django.conf.urls import url
 
 from core_module_fancy_tree_registry_app.views.views import FancyTreeModule
 
 urlpatterns = [
-    re_path(r'module-fancy-tree-registry',
-            FancyTreeModule.as_view(),
-            name='core_module_fancy_tree_registry'),
+    url(r'module-fancy-tree-registry',
+        FancyTreeModule.as_view(),
+        name='core_module_fancy_tree_registry'),
 ]
