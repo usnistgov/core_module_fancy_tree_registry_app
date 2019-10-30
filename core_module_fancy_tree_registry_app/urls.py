ca419214a0783bf0b483ca1cae76d217b80af78b
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-10-30 10:16:31 -0400

    feat: upgrade django 2.2
    Change-Id: I286538706f9732a80785b339c80224afc78f431d

--- core_module_fancy_tree_registry_app/urls.py
+++ core_module_fancy_tree_registry_app/urls.py
@@ -1,11 +1,12 @@
 """ Url router for the fancy tree module
 """
-from django.conf.urls import url
+
+from django.urls import re_path
 
 from core_module_fancy_tree_registry_app.views.views import FancyTreeModule
 
 urlpatterns = [
-    url(r'module-fancy-tree-registry',
-        FancyTreeModule.as_view(),
-        name='core_module_fancy_tree_registry'),
+    re_path(r'module-fancy-tree-registry',
+            FancyTreeModule.as_view(),
+            name='core_module_fancy_tree_registry'),
 ]
