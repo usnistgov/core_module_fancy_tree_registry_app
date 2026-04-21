634374f9e642d5ebc1ab85943b4a6f14c499f02d
Guillaume Sousa <guillaume.sousa@nist.gov>
2026-04-21 21:16:39 +0000

    chore: upgrade 2.22.0
    Change-Id: Iaa693173293734bb2cca3711be8d74f89807057d

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_module_fancy_tree_registry_app",
-    version="2.21.0",
+    version="2.22.0",
     description="Fancy Tree module for the parser core project",
     long_description=long_desc,
     author="NIST IT Lab",
